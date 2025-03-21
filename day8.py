438525c84bbb82f15c22bdff3772da6811b53f4c
imzubich <zailafzal@167gmail.com>
2025-03-21 10:24:40 +0500

    first code class

---
+++ day8.py
@@ -0,0 +1,19 @@
+# ls = ["Karachi", "Lahore", "multan", "faislabad", "sargodha"]
+# ls1 = []
+# for x in ls:
+#     if "a" in x:
+#         ls1.append(x)
+
+# print(ls1)
+
+# ls1 = [print(x) for x in ls if "a" in x]
+
+# ls = ["Karachi", "Lahore", "multan", "faislabad", "sargodha"]
+# ls2 = [x for x in ls if x != "Karachi"]
+# print(ls2)
+
+ls = ["Karachi", "Lahore", "multan", "faislabad", "sargodha"]
+
+ls3 = [x.capitalize() for x in ls]
+
+print(ls3)
