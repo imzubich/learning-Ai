6ae684ed0ed6d97c62da99252d3d0cf643922183
imzubich <zailafzal@167gmail.com>
2025-03-27 10:22:48 +0500

    day12

---
+++ day11assign.py
@@ -0,0 +1,19 @@
+# dic = {
+#     "dic1" : {"name":"Hassan","age":19},
+#     "dic2" : {"name":"Zubair","age":23}
+#     }
+# print(dic)
+
+# dic["dic1"].update({"color":"white"})
+# dic["dic2"].update({"color":"black"})
+# print(dic)
+
+#2nd assignment
+name = input("Please enter your name : ")
+age = input("Please enter your age : ")
+subject = input("Enter your subject : ")
+Student = {
+    "name":name, "age":age, "subject": subject
+}
+
+print(Student)
---
+++ day12.py
@@ -0,0 +1,37 @@
+#functions
+#libraries
+#i) kivi (used for developing android applications)
+#ii) tkinter (used for developing windows applications)
+
+#functions ko likhnay k liye def ka keyword use krna pdta hai 
+
+# def printf():
+#     print("hello werld, How are you.")
+
+# printf()
+
+
+# def func(name = "Zubair"):
+#     print("Hello "+name)
+
+# func()
+
+
+# def func(name):
+#     print("Hello "+name)
+
+# func("Zubair")
+
+
+def func(name, age):
+    print("Hello "+name +" " +str (age))
+
+func("Zubair",30)
+
+#(*) estaric args orbitarory arguments 
+#(**) orbitarory key words arguments
+
+def func(name, *age):
+    print("Hello "+name +" " +str (age[0]))
+
+func("Zubair",30,45)
